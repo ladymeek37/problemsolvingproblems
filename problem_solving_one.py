80824bf52fd0b134dfa729226251ef8d935d43a9
Lady Meek <ladyjmeek@gmail.com>
2022-11-01 20:27:05 -0700

    make function to correctly spell word backwards in problem_solving_one.py

--- problem_solving_one.py
+++ problem_solving_one.py
@@ -1,4 +1,10 @@
 word = "Hello"
 
-for index in range(len(word) -1, -1, -1):
-    print (word[index])
+
+def reverser(word):
+    reversed_word = ""
+    for index in range(len(word) -1, -1, -1):
+        reversed_word += word[index]
+    return reversed_word
+
+print(f'Hello reversed is: {reverser("Hello")}')
