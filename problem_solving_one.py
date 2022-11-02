daa72d943f56036df0b0f7e63747df845cf2db35
Lady Meek <ladyjmeek@gmail.com>
2022-11-02 12:21:01 -0700

    add instructions and add variables to print function

--- problem_solving_one.py
+++ problem_solving_one.py
@@ -1,3 +1,9 @@
+# 1. Choose the word or phrase you want to be reversed.
+# 2. Give that word a variable that will be used later in a function.
+# 3. Write a function that holds a variable for the reversed word, create a for loop that captures each letter for the word backwards to forwards.
+# 4. Return each letter.
+# 5. Print your reversed letters to the terminal using your new reverser function.
+
 word = "Lady Meek"
 
 
