d21a3cdda3edc9a2eff6f23f4260b82c1d99fe68
Lady Meek <ladyjmeek@gmail.com>
2022-11-04 10:54:32 -0700

    add instructions to find a happy number

---
+++ happy_number.py
@@ -0,0 +1,4 @@
+# How to find a happy number:
+# 1. Find the sum of the squares of the digits.
+# 2. Repeat the process until the sum equals one.
+# 3. Use this method to create a function to determine if a number is happy or sad.
