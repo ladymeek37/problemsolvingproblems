a79fc29742e08b042d92971da9b78b620cf5d20d
Lady Meek <ladyjmeek@gmail.com>
2022-11-06 18:25:55 -0800

    create function to discover fibonacci of userimput.

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci (nterms, n1):
+    count = 0
+    n2 = n1 + n1
+    print("Fibonacci sequence:")
+    print(n1)
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
+
+fibonacci(10, (int(input("What number do you want to start your fibonacci sequence?  "))))
